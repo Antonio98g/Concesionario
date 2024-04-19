840ffa40818ac304be62502aa5582dfd983cd5f2
alann616 <156497761+alann616@users.noreply.github.com>
2024-04-18 21:39:47 -0600

    documentación de la clase auto
    se agregaron comentarios que explican el comportamiento de los métodos de la clase auto

--- portafoliopoo/Auto.java
+++ portafoliopoo/Auto.java
@@ -7,6 +7,7 @@ public class Auto {
     private String cliente;
     private Auto siguiente;
 
+    //constructor de la clase auto
     public Auto(String modelo, String marca) {
         this.modelo = modelo;
         this.marca = marca;
@@ -14,6 +15,7 @@ public class Auto {
         this.siguiente = null;
     }
 
+    //setters y getters de la clase auto
     public String getModelo() {
         return modelo;
     }
@@ -54,19 +56,23 @@ public class Auto {
         this.siguiente = siguiente;
     }
 
+    //método toString que muestra los datos del nodo
     @Override
     public String toString() {
         return "Auto{" + "modelo=" + modelo + ", marca=" + marca + ", disponible=" + disponible + ", siguiente=" + siguiente + '}';
     }
-    
+
+    //método toStringBusqueda que solo muestra el modelo del auto que el usuario desea
     public String toStringBusqueda() {
         return "Auto:" + "modelo=" + modelo + ", marca=" + marca + ", disponible=" + disponible ;
     }
-    
+
+    //método toStringVendidos que muestra solo a los autos que ya han sido comprados
     public String toStringVendidos() {
         return "Auto:" + "modelo=" + modelo + ", marca=" + marca + ", cliente=" + cliente ;
     }
-    
+
+    //método toStringDevuelto que muestra los autos que algún cliente ha devuelto
     public String toStringDevuelto() {
         return "Auto:" + "modelo=" + modelo + ", marca=" + marca + ", cliente= pendiente" + ", disponible=" + disponible ;
     }
